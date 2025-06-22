45395cbb407121deba4e48b8ac39b3e53720c1e6
vuqz165 <vwbwkjwekewkw@gmail.com>
2025-06-22 18:59:54 +0200

    server.py

--- server.py
+++ server.py
@@ -21,10 +21,10 @@ async def websocket_endpoint(websocket: WebSocket):
         while True:
             data = await websocket.receive_text()
             for client in clients:
-                if client != websocket:
-                    await client.send_text(data)
+                await client.send_text(data)
     except WebSocketDisconnect:
-        clients.remove(websocket)
+        if websocket in clients:
+            clients.remove(websocket)
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=10000)
