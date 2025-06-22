036883b0f85a8ef842591cb5badae602c6f1ef6b
vuqz165 <vwbwkjwekewkw@gmail.com>
2025-06-22 18:39:03 +0200

    server.py

--- server.py
+++ server.py
@@ -3,38 +3,53 @@ import threading
 import os
 
 HOST = '0.0.0.0'
-PORT = int(os.environ.get("PORT", 5000))  # required for Render
+PORT = int(os.environ.get("PORT", 5000))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
 clients = []
+clients_lock = threading.Lock()  # To safely edit client list
 
 def broadcast(message, sender):
-    for client in clients:
-        if client != sender:
-            try:
-                client.send(message)
-            except:
-                client.close()
-                clients.remove(client)
+    with clients_lock:
+        for client in clients.copy():
+            if client != sender:
+                try:
+                    client.send(message)
+                except:
+                    try:
+                        client.close()
+                    except:
+                        pass
+                    if client in clients:
+                        clients.remove(client)
 
 def handle(client):
-    while True:
-        try:
+    try:
+        while True:
             msg = client.recv(1024)
+            if not msg:
+                break
             broadcast(msg, client)
-        except:
-            clients.remove(client)
+    except:
+        pass
+    finally:
+        with clients_lock:
+            if client in clients:
+                clients.remove(client)
+        try:
             client.close()
-            break
+        except:
+            pass
 
 def receive():
     print(f"Server started on {HOST}:{PORT}")
     while True:
         client, _ = server.accept()
-        clients.append(client)
-        threading.Thread(target=handle, args=(client,)).start()
+        with clients_lock:
+            clients.append(client)
+        threading.Thread(target=handle, args=(client,), daemon=True).start()
 
 receive()
